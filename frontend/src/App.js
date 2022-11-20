e615c5dc412ee0738c1bbcce7fd509d5c26d0975
louisthms <louistwin78thms@gmail.com>
2022-11-20 17:03:21 +0100

    add react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
+        <Router>
+          <div className="navbar">
+            <ul>
+              <li><Link to={'/'} className="nav-link"> Home </Link></li>
+              <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
+              <li><Link to={'/about'} className="nav-link">About</Link></li>
+            </ul>
+          </div>
+          <Routes>
+            <Route exact path='/' />
+            <Route path='/contact' />
+            <Route path='/about' />
+          </Routes>
+        </Router>
+      </div>
 
     </div>
   );
