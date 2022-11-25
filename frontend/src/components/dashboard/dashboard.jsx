c7b51785d87d0b0c964146dae0990abae2813917
louisthms <louistwin78thms@gmail.com>
2022-11-25 18:11:05 +0100

    display charts

--- frontend/src/components/dashboard/dashboard.jsx
+++ frontend/src/components/dashboard/dashboard.jsx
@@ -3,6 +3,9 @@ import './dashboard.css'
 import Converter from '../converter/converter';
 import Crypto from '../crypto/crypto';
 import Stock from '../stock/stock';
+import CryptoConv from '../converter/cryptoConv';
+import Charts from '../chart/chart';
+import ChartDay from '../chart/chartDay';
 
 const dashboard = () => {
     return (
@@ -10,9 +13,15 @@ const dashboard = () => {
             <h2>Dashboard</h2>
             <div className="card">
                 <Converter />
+                <CryptoConv />
                 <Crypto />
                 <Stock />
             </div>
+            <div className="charts">
+                <Charts />
+                <ChartDay />
+            </div>
+
 
         </div>
     )
