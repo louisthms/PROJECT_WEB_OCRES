f011c9b123f404735c32bc2f69266b8849dedf27
louisthms <louistwin78thms@gmail.com>
2022-11-25 20:25:23 +0100

    add right side

---
+++ frontend/src/components/rightSide/rightSide.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './rightSide.css'
+
+
+const rightSide = () => {
+    return (
+        <div className="rightside">
+            <h3>My Account</h3>
+            <div className="card">
+
+            </div>
+        </div>
+    )
+}
+
+export default rightSide
