3dfbeaaf9644a34504cbdc5e37024ee6cfeb052d
louisthms <louistwin78thms@gmail.com>
2022-11-27 15:34:02 +0100

    add one link

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -6,9 +6,10 @@ const navbar = () => {
     return (
         <div className="navbar">
             <ul>
-                <li><Link to={'/'} className="nav-link"> Home </Link></li>
-                <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
-                <li><Link to={'/about'} className="nav-link">About</Link></li>
+                <li><Link to={'/'} className="nav-link">Home</Link></li>
+                <li><Link to={'/recherche'} className="nav-link">Recherche</Link></li>
+                <li><Link to={'/contact'} className="nav-link">Contacts</Link></li>
+                <li><Link to={'/virement'} className="nav-link">Virements</Link></li>
             </ul>
         </div>
     )
