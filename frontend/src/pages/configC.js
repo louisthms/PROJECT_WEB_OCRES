c8d9e94e1bda57c4bbbe5b47d28af5365f6367a5
Gayathrie-Sr <srikasthu.gaya@gmail.com>
2022-11-27 15:32:46 +0100

    ajout composant pages

---
+++ frontend/src/pages/configC.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ConfigC() {
+    return (
+        <div>
+            Supprimer er un cSontact
+        </div>
+    );
+}
+
+export default ConfigC;
---
+++ frontend/src/pages/configD.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ConfigD() {
+    return (
+        <div>
+            Supprimer er un cSontact
+        </div>
+    );
+}
+
+export default ConfigD;
