1b9396b20af8910de015a511f14d6eadf38cd73a
louisthms <louistwin78thms@gmail.com>
2022-11-29 19:22:19 +0100

    add others pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/home/home'
 import Recherche from './components/recherche/recherche'
-
+import Contacts from './components/contacts/contacts'
+import Virements from './components/virements/virements'
 
 function App() {
   return (
@@ -11,11 +12,10 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/recherche' element={<Recherche />} />
-          <Route path='/contact' />
-          <Route path='/virement' />
+          <Route path='/contact' element={<Contacts />} />
+          <Route path='/virement' element={<Virements />} />
         </Routes>
       </Router>
-
     </div>
   );
 }
---
+++ frontend/src/components/virements/virements.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../navbar/navbar';
+import './virements.css';
+
+const virements = () => {
+    return (
+        <div className="virements">
+            <Navbar />
+        </div>
+    )
+}
+
+export default virements
