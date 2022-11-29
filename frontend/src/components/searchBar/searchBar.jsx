31b9b27362c251cdf99a76d7a523d4b3b0499444
louisthms <louistwin78thms@gmail.com>
2022-11-29 13:49:00 +0100

    recherche done

--- frontend/src/components/searchBar/searchBar.jsx
+++ frontend/src/components/searchBar/searchBar.jsx
@@ -3,11 +3,17 @@ import './searchBar.css'
 import React from 'react';
 import axios from "axios";
 import { useState } from "react";
+import Chart from 'react-apexcharts'
 
 const SearchBar = () => {
 
 
     const [data, setData] = useState('');
+    const [stockX, setStockX] = useState([]);
+    const [stockY, setStockY] = useState([]);
+
+    let stockChartXValues = [];
+    let stockChartYValues = [];
 
     const inputHandler = (e) => {
         setData(e.target.value);
@@ -19,29 +25,87 @@ const SearchBar = () => {
             .then((response) => {
                 console.log(response.data);
                 setData(response.data['Meta Data']['2. Symbol']);
+                for (var key in response.data['Time Series (5min)']) {
+                    stockChartXValues.push(key);
+                    setStockX(stockChartXValues);
+                    stockChartYValues.push(response.data['Time Series (5min)'][key]['1. open']);
+                    setStockY(stockChartYValues);
+                }
             })
             .catch((error) => {
                 console.log(error);
             });
     };
 
-    console.log(data);
-
     return (
-        <div className="search">
-            <div className="searchInputs">
-                <input
-                    type="text"
-                    placeholder="Search"
-                    value={data}
-                    onChange={inputHandler}
-                />
+        <div className="searchBar">
+            <div className="search">
+                <div className="searchInputs">
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        value={data}
+                        onChange={inputHandler}
+                    />
 
-                <div className="searchIcon" onClick={GetAllData}>
-                    <UilSearch />
+                    <div className="searchIcon" onClick={GetAllData}>
+                        <UilSearch />
+                    </div>
+                </div>
+            </div>
+            <div className="search">
+                <div className="searchChart">
+                    {data != "" ? <h2>{data}</h2> : <h3></h3>}
+                    <Chart options={{
+                        stroke: {
+                            curve: 'smooth',
+                            width: 2,
+                            colors: ["#ff929f"],
+                        },
+                        chart: {
+                            id: 'apexchart-example',
+                            toolbar: {
+                                show: false
+                            },
+                            zoom: {
+                                enabled: false
+                            }
+                        },
+                        grid: {
+                            show: false,
+                        },
+                        dataLabels: {
+                            enabled: false
+                        },
+                        yaxis: {
+                            labels: {
+                                show: false,
+                                style: {
+                                    colors: 'white',
+                                    fontSize: '12px'
+                                },
+                            }
+                        },
+                        xaxis: {
+                            categories: stockX,
+                            type: 'datetime',
+                            labels: {
+                                show: true,
+                                style: {
+                                    colors: 'white',
+                                    fontSize: '12px'
+                                },
+                            }
+                        },
+                    }}
+                        series={[{
+                            name: 'value',
+                            data: stockY,
+                        }]} type="area" />
                 </div>
             </div>
         </div>
+
     )
 }
 
