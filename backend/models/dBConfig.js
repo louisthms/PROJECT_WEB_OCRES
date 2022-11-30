b0656956ad464db6cb0dc59613467fb76139eb92
Gayathrie-Sr <srikasthu.gaya@gmail.com>
2022-11-30 09:47:26 +0100

    Config database

---
+++ backend/models/dBConfig.js
@@ -0,0 +1,12 @@
+/*
+const mongoose = require('mongoose');
+
+mongoose.connect(
+    "mongodb://localhost:27017/contact",
+    {useNewUrlParser: true, useUnifiedTopology: true },
+    (err) => {
+        if (!err)console.log("Mongodb connected");
+        else console.log("Connection error :" + err);
+    }
+)
+*/
